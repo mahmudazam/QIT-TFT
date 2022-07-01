7a4b0c1b39ce7e042cfbb5a34cee1c8c2e25cae6
Mahmud Azam <mahmud.azam@usask.ca>
2022-06-30 20:55:18 -0400

    Start review

--- Thick_TQFTs_and_Quantum_Information/pre.mk
+++ Thick_TQFTs_and_Quantum_Information/pre.mk
@@ -1,15 +1,3 @@
 
 #CODE_DEPS=$(shell ls -1 ${PRJWD}/*/*.sage | sed 's:\(.*\):${PRJWD}/build/\1.out:g')
-BIB_DEPS=$(shell ls -1 *.bib)
-STY_DEPS=$(shell ls -1 *.sty)
-TEX_DEPS=$(shell ls -1 *.tex)
-
-%.pdf: %.tex
-	pdflatex -interaction nonstopmode\
-           $<
-	bibtex $*
-	pdflatex -interaction nonstopmode\
-           $<
-	pdflatex -interaction nonstopmode\
-           $<
 
